20984626848191afaccb2223d740bd8575f78684
Tim <timburnsowlmtn@gmail.com>
2023-05-03 12:44:57 -0400

    Terraform integration build fixes

--- src/terraform/modules/snowflake-tables/table.tf
+++ src/terraform/modules/snowflake-tables/table.tf
@@ -1,7 +1,5 @@
 
 resource "snowflake_table" "table" {
-  for_each = var.domain_model["properties"]
-
   database            = var.snowflake_database
   schema              = var.landing_zone_schema
   name                = var.table_name
@@ -11,9 +9,9 @@ resource "snowflake_table" "table" {
   change_tracking     = false
 
   column {
-    name     = each.value["COLUMN_NAME"]
-    type     = each.value["DATA_TYPE"]
-    nullable = each.value == "YES" ? true : false
+    name     = "JSON_OBJECT"
+    type     = "VARIANT"
+    nullable = true
 
   }
 
