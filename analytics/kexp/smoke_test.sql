81d1b05f3647a2148d20dba5eeb882d97feeb154
Tim <timburnsowlmtn@gmail.com>
2023-05-05 09:21:53 -0400

    Complete the notification piece on the build

--- analytics/kexp/smoke_test.sql
+++ analytics/kexp/smoke_test.sql
@@ -1,12 +1,31 @@
 select *
 from LANDING_ZONE.IMPORT_LOG;
 
-select to_date(START_TIME), count(*)
+----------------------------------------------------------- PIPE_IMPORT_SHOW
+
+use schema LANDING_ZONE;
+select parse_json(system$pipe_status('PIPE_IMPORT_SHOW'));
+
+select to_date(START_TIME), max(START_TIME), count(*)
 from LANDING_ZONE.IMPORT_SHOW
 group by to_date(START_TIME)
 order by to_date(START_TIME) desc;
 
-select to_date(airdate), count(*)
+select *
+from LANDING_ZONE.IMPORT_SHOW
+where SHOW_ID is not null
+order by START_TIME DESC
+limit 20;
+
+----------------------------------------------------------- PIPE_IMPORT_PLAYLIST
+select parse_json(system$pipe_status('PIPE_IMPORT_PLAYLIST'));
+
+select to_date(airdate), max(AIRDATE), count(*)
 from LANDING_ZONE.IMPORT_PLAYLIST
 group by to_date(airdate)
 order by to_date(airdate) desc;
+
+select *
+from LANDING_ZONE.IMPORT_PLAYLIST
+order by AIRDATE DESC
+limit 20;
